91ffd1d87e022fcf84bc56211b552320dbae9923
changkaiguo <gogechang@163.com>
2025-10-15 10:03:03 +0800

    调整文件目录架构，修复事件类型提示问题，修复打包问题优化打包结构和类型提示

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-import threeScene from "./lib/baseScene";
-
-export default threeScene;
+import ThreeSceneCore from "./src/core/baseScene";
+import ThreeSceneVue from "./components/threeScene.vue";
+import type { ThreeSceneOptions } from "./src/types/threeScene";
+export { ThreeSceneVue, ThreeSceneCore, ThreeSceneOptions };
+export default ThreeSceneCore;
